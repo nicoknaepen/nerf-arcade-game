31b617ef4b3f8edeb4cc7b9c6a33d440b1727975
Nico Knaepen <nico.knaepen@gmail.com>
2016-12-11 15:29:55 +0100

    Refactored code and provided first init melody and led blink.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,25 +1,51 @@
-int buttonState = 0;
+int hitState = 0;
 int led2Active = 0;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(2, OUTPUT);
   pinMode(7, OUTPUT);
-  digitalWrite(2, LOW);
+  initTones();
+  initLeds();
 }
 
 void loop() {
-  buttonState = digitalRead(8);
+  objectHitVerification(8, 2);
+}
 
-  if (buttonState == HIGH) {
-    digitalWrite(2, HIGH);
+int objectHitVerification(int object, int led) {
+  hitState = digitalRead(object);
+
+  if (hitState == HIGH) {
     if (led2Active == 0) {
-      tone(7, 500, 100);
-      delay(120);
-      tone(7, 500, 100);
-      delay(120);
-      tone(7, 750, 250);
-      led2Active = 1;
+      objectHit(led);
     }
   }
 }
+
+int objectHit(int led) {
+  objectHitTone(500,750);  
+  digitalWrite(2, HIGH);
+  led2Active = 1;  
+}
+
+int objectHitTone(int tone1, int tone2){
+  tone(7, tone1, 100);
+  delay(120);
+  tone(7, tone1, 100);
+  delay(120);
+  tone(7, tone2, 250);
+  delay(120);
+}
+
+void initTones() {
+  objectHitTone(500,750);  
+  objectHitTone(500,1000);  
+}
+
+void initLeds() {
+  digitalWrite(2, HIGH);
+  delay(120);
+  digitalWrite(2, LOW); 
+}
+
