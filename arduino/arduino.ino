695ab8bbac37c468d2fad9791a3049bb13c62017
Nico Knaepen <nico.knaepen@gmail.com>
2016-12-11 15:07:23 +0100

    Generate a one time melody once a button is pressed.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,25 @@
 int buttonState = 0;
+int led2Active = 0;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(2, OUTPUT);
-  pinMode(8, INPUT);
+  pinMode(7, OUTPUT);
   digitalWrite(2, LOW);
 }
 
 void loop() {
   buttonState = digitalRead(8);
- 
+
   if (buttonState == HIGH) {
     digitalWrite(2, HIGH);
+    if (led2Active == 0) {
+      tone(7, 500, 100);
+      delay(120);
+      tone(7, 500, 100);
+      delay(120);
+      tone(7, 750, 250);
+      led2Active = 1;
+    }
   }
 }
