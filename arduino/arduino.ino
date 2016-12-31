f1952864de85bf0f2b22c3b4526a346d968fbad0
Nico Knaepen <nico.knaepen@gmail.com>
2016-12-31 16:01:06 +0100

    Hit only once by nico.knaepen: Added extra check if object was already hit and added victory song after everything is hit.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,7 @@
 int hitState = 0;
 int arrObjects[] = {8, 9, 10, 11, 12};
 int arrLeds[] = {2, 3, 4, 5, 6};
+int arrHits[] ={0, 0, 0, 0, 0};
 int i;
 
 void setup() {
@@ -25,13 +26,41 @@ void loop() {
 int objectHitVerification(int i) {
   hitState = digitalRead(arrObjects[i]);
   if (hitState == HIGH) {
-    objectHit(i);
+    if (arrHits[i] == 0) {
+      objectHit(i);
+    }
   }
 }
 
 int objectHit(int i) {
-  objectHitTone(500,750);  
+  objectHitTone(500,750);
   digitalWrite(arrLeds[i], HIGH);
+  arrHits[i] = 1;
+  verifyAllObjectsAreHit();
+}
+
+int verifyAllObjectsAreHit() {
+  int totalHits = 0;
+  for (i = 0; i < 5; i = i + 1) {
+    totalHits += arrHits[i];
+  }
+  if (totalHits == 5) {
+    victoryAndReset();
+  }
+}
+
+int victoryAndReset() {
+  objectHitTone(500,750);  
+  objectHitTone(500,1000);  
+  objectHitTone(1000,1250);  
+  objectHitTone(1250,1500);  
+  objectHitTone(2000,2000); 
+  // Initialize pinMode for leds and buttons/objects to hit.
+  for (i = 0; i < 5; i = i + 1) {
+    digitalWrite(arrLeds[i], LOW);
+    arrHits[i] = 0;
+  }
+
 }
 
 int objectHitTone(int tone1, int tone2){
