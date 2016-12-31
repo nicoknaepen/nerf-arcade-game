e0ff6d603f72bfc8555f67df093b4965096f2a0e
Nico Knaepen <nico.knaepen@gmail.com>
2016-12-31 15:34:00 +0100

    Bug hit objects by nico.knaepen: Fixed bug for hitting objects 3-5.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,6 @@
 int hitState = 0;
 int arrObjects[] = {8, 9, 10, 11, 12};
 int arrLeds[] = {2, 3, 4, 5, 6};
-int arrHitObjects[] = {};
 int i;
 
 void setup() {
@@ -9,10 +8,7 @@ void setup() {
   for (i = 0; i < 5; i = i + 1) {
     pinMode(arrLeds[i], OUTPUT);
     pinMode(arrObjects[i], INPUT);
-    arrHitObjects[i] = 0;
   }
-  Serial.begin(9600);
-  Serial.print("Hello again");
   // Initialize pinMode for the buzzer.
   pinMode(7, OUTPUT);
   // Test the tones and the leds.
@@ -29,10 +25,7 @@ void loop() {
 int objectHitVerification(int i) {
   hitState = digitalRead(arrObjects[i]);
   if (hitState == HIGH) {
-    if (arrHitObjects[i] == 0) {
-      objectHit(i);
-      arrHitObjects[i] = 1;
-    }
+    objectHit(i);
   }
 }
 
